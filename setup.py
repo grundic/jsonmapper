3e414edf631406ab3f8e8627071b7c46ba7a2050
Grigory Chernyshev <systray@yandex.ru>
2017-05-24 10:33:10 +0200

    Reformat `setup.py` file.

--- setup.py
+++ setup.py
@@ -9,15 +9,14 @@
 
 from setuptools import setup
 
-
 setup(
-    name = 'JSONMapper',
-    version = '0.1',
-    description = 'Python JSON mapper',
-    long_description = '',
-    author = 'Matt Good',
-    author_email = 'matt@matt-good.net',
-    license = 'BSD',
-    url = '',
-    packages = ['jsonmapper'],
+    name='JSONMapper',
+    version='0.1',
+    description='Python JSON mapper',
+    long_description='',
+    author='Matt Good',
+    author_email='matt@matt-good.net',
+    license='BSD',
+    url='',
+    packages=['jsonmapper'],
 )
